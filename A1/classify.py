fda7a31d2f295e77ca06adf9c335a2e4593568c1
Sandro Young <ord-nas@users.noreply.github.com>
2017-02-12 22:51:27 -0500

    Added cross-validation and significance testing.

--- A1/classify.py
+++ A1/classify.py
@@ -3,6 +3,31 @@ import re
 import sys
 import os
 
+from scipy import stats
+
+models = ["weka.classifiers.functions.SMO",
+          "weka.classifiers.bayes.NaiveBayes",
+          "weka.classifiers.trees.J48"]
+
+confusion_matrix_pattern = r"""=== Error on test data ===
+.*
+=== Confusion Matrix ===
+
+\s*a\s*b\s*<-- classified as
+\s*([0-9]+)\s*([0-9]+)\s*\|\s*a = 0
+\s*([0-9]+)\s*([0-9]+)\s*\|\s*b = 4
+"""
+
+def average(values):
+    if not values:
+        return 0 # No values; avoid division by zero
+    else:
+        # Otherwise compute average
+        return float(sum(values)) / len(values)
+
+def flatten(lsts):
+    return sum(lsts, [])
+
 def run(args, pipe_output=False):
     print " ".join(args)
     p = subprocess.Popen(args, stdout = subprocess.PIPE if pipe_output else sys.stdout)
@@ -19,11 +44,8 @@ def get_test_accuracy(out):
     return accuracy
 
 def part_31(output_folder):
+    global models
     print "\n3.1 CLASSIFIERS\n"
-
-    models = ["weka.classifiers.functions.SMO",
-              "weka.classifiers.bayes.NaiveBayes",
-              "weka.classifiers.trees.J48"]
 
     output_file = os.path.join(output_folder, "3.1output.txt")
     f = open(output_file, "w")
@@ -106,7 +128,7 @@ def part_33(output_folder, model):
     for n in [500, 10000]:
         print "Computing information gain on %d examples ..." % n
         f.write("Information gain results for n=%d\n" % n)
-        f.write("===============================%s\n" % "".join("=" for _ in str(n)))
+        f.write("===============================%s\n" % ("=" * len(str(n))))
         out = run(["sh",
                    "WEKA/infogain.sh",
                    os.path.join(output_folder, "train_%d.arff" % n)],
@@ -114,7 +136,117 @@ def part_33(output_folder, model):
         f.write(out)
         
     f.close()
+
+def part_34(output_folder):
+    global models
+    print "\n3.4 CROSS-VALIDATION\n"
+
+    output_file = os.path.join(output_folder, "3.4output.txt")
+    out_file = open(output_file, "w")
+    
+    full_arff_file = os.path.join(output_folder, "train.arff")
+    with open(full_arff_file, "r") as f:
+        in_header = True
+        header = ""
+        zero = []
+        four = []
+        for line in f:
+            if in_header:
+                header += line
+            else:
+                if line.strip().split(",")[-1] == "0":
+                    zero.append(line)
+                else:
+                    four.append(line)
+            if line == "@data\n":
+                in_header = False
+
+    num_folds = 10
+    print len(zero), len(four)
+    print "ZERO:", zero[0], "::::", zero[-1]
+    print "FOUR:", four[0], "::::", four[-1]
+    assert len(zero) == len(four)
+    assert len(zero) % num_folds == 0
+    assert len(four) % num_folds == 0
+    fold_size = len(zero) / num_folds
+    zero_folds = [zero[i*fold_size:(i+1)*fold_size] for i in xrange(num_folds)]
+    four_folds = [four[i*fold_size:(i+1)*fold_size] for i in xrange(num_folds)]
+
+    accuracies = []
+    for fold in xrange(num_folds):
+        # Write the test and train .arff files for this fold
+        print "Creating training data for fold %d ..." % fold
+        test_file = os.path.join(output_folder, "test_fold_%d.arff" % fold)
+        with open(test_file, "w") as f:
+            f.write(header)
+            f.write("".join(zero_folds[fold]))
+            f.write("".join(four_folds[fold]))
+        train_file = os.path.join(output_folder, "train_fold_%d.arff" % fold)
+        with open(train_file, "w") as f:
+            f.write(header)
+            f.write("".join(flatten(zero_folds[:fold] + zero_folds[fold+1:])))
+            f.write("".join(flatten(four_folds[:fold] + four_folds[fold+1:])))
         
+        out_file.write("Fold %d results\n" % fold)
+        out_file.write("=====%s========\n\n" % ("=" * len(str(fold))))
+        
+        out_file.write("Classifier                          | Accuracy | Precision (class 0) | Precision (class 4) | Precision (average) | Recall (class 0) | Recall (class 4) | Recall (average) \n")
+        out_file.write("------------------------------------+----------+---------------------+---------------------+---------------------+------------------+------------------+------------------\n")
+
+        # Now train each of the models
+        
+        fold_accuracies = []
+        for model in models:
+            print "Running classifier %s on fold %d ..." % (model, fold)
+            out = run(["java",
+                       "-cp",
+                       "WEKA/weka.jar",
+                       model,
+                       "-t",
+                       train_file,
+                       "-T",
+                       test_file],
+                      pipe_output=True)
+            m = re.search(confusion_matrix_pattern, out, flags=re.DOTALL)
+            assert m
+            TN = int(m.group(1))
+            FP = int(m.group(2))
+            FN = int(m.group(3))
+            TP = int(m.group(4))
+            accuracy = float(TP+TN) / float(TP+FP+TN+FN)
+            fold_accuracies.append(accuracy)
+            P_precision = float(TP) / float(TP + FP)
+            P_recall = float(TP) / float(TP + FN)
+            N_precision = float(TN) / float(TN + FN)
+            N_recall = float(TN) / float(TN + FP)
+            avg_precision = (P_precision + N_precision) / 2.0
+            avg_recall = (P_recall + N_recall) / 2.0
+
+            out_file.write("%35s | %8f | %19f | %19f | %19f | %16f | %16f | %16f \n" % (
+                model, accuracy, N_precision, P_precision, avg_precision, N_recall, P_recall, avg_recall))
+        out_file.write("\n")
+        accuracies.append(fold_accuracies)
+
+    # Transpose accuracies!
+    accuracies = zip(*accuracies)
+    print "accuracies:", accuracies
+
+    # Test for significance
+    level = 0.05
+    out_file.write("Significance tests\n")
+    out_file.write("==================\n\n")
+    for i in xrange(len(models)):
+        for j in xrange(i+1,len(models)):
+            direction = "better" if average(accuracies[i]) > average(accuracies[j]) else "worse"
+            (_, p_value) = stats.ttest_rel(accuracies[i], accuracies[j])
+            out_file.write("%s vs. %s: p-value %f\n"% (models[i], models[j], p_value))
+            out_file.write("   (%s the null hypothesis at %f significance level)\n" % (
+                "REJECT" if p_value < level else "CANNOT REJECT", level))
+            out_file.write("   (conclude that %s is %s%s than %s)\n" % (
+                models[i], "" if p_value < level else "not significantly ",
+                direction, models[j]))
+    out_file.close()
+    
 def main(args):
     if len(args) != 3:
         print "Usage: python classify.py output_folder student_number"
@@ -156,9 +288,10 @@ def main(args):
          os.path.join(output_folder, "test.twt"),
          os.path.join(output_folder, "test.arff")])
 
-    best_model = part_31(output_folder)
-    part_32(output_folder, best_model)
-    part_33(output_folder, best_model)
+    #best_model = part_31(output_folder)
+    #part_32(output_folder, best_model)
+    #part_33(output_folder, best_model)
+    part_34(output_folder)
 
             
 if __name__ == "__main__":
