0bb7ad44278a64804fac31f30da0254bdfa15a8b
Sandro Young <ord-nas@users.noreply.github.com>
2017-02-13 03:32:17 -0500

    Cleanup and fixes.
    twtt.py should now be basically finished and ready to go.

--- A1/twtt.py
+++ A1/twtt.py
@@ -1,10 +1,14 @@
 import re
 import sys
 import itertools
+import HTMLParser
 
 import NLPlib
 
+# Parse the comma-separated-values line and pull out the tweet and the
+# polarity. Return a tuple of (tweet, polarity)
 def extract_tweet_and_polarity(csv_line):
+    # Note: we can't simple split on commas because commas can appear in tweets!
     m = re.match(r'^"([^,]*)","[^,]*","[^,]*","[^,]*","[^,]*","(.*)"$', csv_line)
     assert m
     return (m.group(2), int(m.group(1)))
@@ -18,31 +22,48 @@ def twtt1(tweet):
     # <img onclick="alert('5 > 4');"/>
     #                        ^
     #                        This is *not* the end of the tag
-    pattern = r"""<(?:[^'>"]|"[^"]*"|'[^']*')+>"""
+    pattern = r"""<(?:[^>'"]|"[^"]*"|'[^']*')+>"""
+    # Pattern is as follows:
+    # <                    Match start of HTML tag
+    # (?:                  Then match one or more of the following (in a
+    #                      non-capturing group so that re.split won't keep it):
+    # [^>'"]                   A character that *isn't* the end of the HTML
+    #                          tag, and also *isn't* the beginning of a quoted
+    #                          attribute
+    # |                        OR
+    # "[^"]*"                  An attribute surrounded by double quotes
+    # |                        OR
+    # '[^']*'                  An attribute surrounded by single quotes
+    # )+                   (end of group ... match one or more of these)
+    # >                    Finally, match the end of the tag
     return ''.join(re.split(pattern, tweet))
 
 # Decode html character code
-# TODO: HTML CODES THAT ARE NOT NUMBERS
-# TODO: CONSIDER USING BUILT-IN PYTHON STUFF?
 def twtt2(tweet):
+    global html_parser
+    # I had trouble just using python's built-in HTML unescape method on the
+    # entire string; weird unicode errors. But if I just run it on pieces that
+    # look like html escape sequences, seems fine.
     while True:
-        m = re.search(r"&#([0-9]{2,3});", tweet)
+        m = re.search(r"(&[#A-Za-z0-9]+;)", tweet)
         if not m:
             break
-        num = int(m.group(1))
-        assert 32 <= num <= 127
-        tweet = tweet[:m.start()] + chr(num) + tweet[m.end():]
+        #print "ORIGINAL:", m.group(1)
+        replacement = str(html_parser.unescape(m.group(1)))
+        #print "REP:", replacement
+        tweet = tweet[:m.start()] + replacement + tweet[m.end():]
     return tweet
 
 # Remove tokens which begin with www or http
 def twtt3(tweet):
-    # We are going to try to roughly match Twitter's behaviour when looking for
-    # URLs. Twitter actually looks for more than just a simple http or www to
-    # identify URLs, but since the assignment specifically asks us to just look
-    # for tokens starting http or www, we'll do that. However, we still need to
+    # Twitter actually looks for more than just a simple http or www to identify
+    # URLs, but since the assignment specifically asks us to just look for
+    # tokens starting http or www, we'll do that. However, we still need to
     # determine what counts as a token. In particular, what counts as the
-    # *start* of a URL token, and what counts as the *end* of a URL token? This
-    # is where we will take inspiration from Twitter's actual behaviour.
+    # *start* of a URL token, and what counts as the *end* of a URL token? To
+    # answer this, we are going to try to roughly match Twitter's actual
+    # behaviour - in other words, when Twitter creates a hyperlink in a tweet,
+    # how does it determine wher the URL begins and ends?
     pattern = r"""\b(?:http|www)[^\s`^(){}<>\"]*[^\s`^(){}<>\".,!?\[\]~@$%&*|:;'"]"""
     # Pattern is as follows:
     # \b
@@ -55,32 +76,52 @@ def twtt3(tweet):
     #                      a few special characters
     # [^\s`^(){}<>\".,!?\[\]~@$%&*|:;'"]
     #                      There are additional restrictions on what is allowed
-    #                      to be the *last* chracter of the URL
-    #print re.split(pattern, tweet, flags=re.IGNORECASE)
+    #                      to be the *last* chracter of the URL - for example,
+    #                      if your tweet is "I love http://site.com!", the
+    #                      exclamation point will not be considered part of the
+    #                      URL, even though exclamation points are allowed at
+    #                      other places in the URL.
     return ''.join(re.split(pattern, tweet, flags=re.IGNORECASE))
 
 # Remove @ from Twitter user names and # from hash tags.
 def twtt4(tweet):
+    # The most complicated aspect of this is: how do we know when an @ or #
+    # symbol is part of a username/hashtag, instead of just regular text?
+    # Examples which are NOT usernames or hashtags:
+    #      name@gmail.com
+    #      @@words
+    #      $@whatever
+    #      #1
+    #      &#stuff
+    # Examples which ARE usernames or hashtags:
+    #      (@name)
+    #      ...#hashtag
+    #      #1a
+    #      :@you
+    # So the problem is not entirely straighforward.
     handle_pattern = r"(^|[^A-Za-z0-9_!@#$%&*])@([A-Za-z0-9_])"
+    # Pattern breakdown:
     # (^|[^A-Za-z0-9_!@#$%&*])
     #                      Immediately preceeding the twitter handle, there can
     #                      be either the start of the tweet, or any character
     #                      *except* for a few special ones. We use a capturing
     #                      group so that re.split will keep this character.
     # @
-    #                      Then comes the @ symbol itslef.
+    #                      Then comes the @ symbol itself.
     # ([A-Za-z0-9_])
     #                      The following characters are the only ones allowed
     #                      to be in a twitter handle, so must directly follow
     #                      the @ symbol. Again, we use a capturing group to
     #                      keep these characters, we only want to throw away
     #                      the @ symbol.
-    #print re.split(handle_pattern, tweet)
     tweet = ''.join(re.split(handle_pattern, tweet))
 
     # Hashtag is similar, but a wider range of characters are allowed to appear
-    # before the hashtag.
-    hashtag_pattern = r"(^|[^A-Za-z0-9_&])#([A-Za-z0-9_])"
+    # before the hashtag. Further, although numbers and underscores may be
+    # included in a hashtag, hashtags need to include *at least* one number. So
+    # the following are not hashtags: #1 or #_ or #12345_6789
+    hashtag_pattern = r"(^|[^A-Za-z0-9_&])#([0-9_]*)([A-Za-z])"
+    # Pattern breakdown:
     # (^|[^A-Za-z0-9_&])
     #                      Immediately preceeding the hashtag, there can
     #                      be either the start of the tweet, or any character
@@ -88,22 +129,26 @@ def twtt4(tweet):
     #                      group so that re.split will keep this character.
     # #
     #                      Then comes the # symbol itself.
-    # ([A-Za-z0-9_])
-    #                      The following characters are the only ones allowed
-    #                      to be in a twitter hashtag, so must directly follow
-    #                      the # symbol. Again, we use a capturing group to
-    #                      keep these characters, we only want to throw away
-    #                      the # symbol.
-    #print re.split(hashtag_pattern, tweet)
+    # ([0-9_]*)
+    #                      Hashtags may begin with any number of digits or
+    #                      underscores. Again, we use a capturing group to keep
+    #                      these characters, we only want to throw away the #
+    #                      symbol.
+    # ([A-Za-z])
+    #                      A hashtag is required to contain at least one letter
+    #                      somewhere.
     tweet = ''.join(re.split(hashtag_pattern, tweet))
 
     return tweet
 
-# TODO: check for lowercase after acronyms too?
+# Break tweet into sentences, and put each sentence on its own line.
 def twtt5(tweet):
     global abbrevs, non_terminal_abbrevs
 
-    # Returns None if not, otherwise returns the abbrev
+    # First, a bunch of helper functions:
+
+    # Check if character i in the given tweet is the end of an abbreviation.
+    # If so, returns the abbreviation. If not, returns None.
     def get_abbrev(tweet, i):
         # Look for each abbreviation in turn.
         for a in abbrevs:
@@ -116,26 +161,25 @@ def twtt5(tweet):
                     # Oops, not an abbreviation!
                     continue
                 # Okay we have an abbreviation!
-                # # If it's a non-terminal abbrev (i.e. one that doesn't normally
-                # # occur at the end of sentence), ignore it.
-                # if tweet[i-len(a)+1:i+1] in non_terminal_abbrevs:
-                #     return False # Non-terminal abbrev
-                return tweet[i-len(a)+1:i+1] # Found a terminal abbrev!
+                return tweet[i-len(a)+1:i+1]
         return None # None of the abbrevs matched
-    # x = map(lambda i: is_end_of_abbrev(tweet, i), xrange(len(tweet)))
-    # return x
-    # import itertools
-    # return map(lambda b: b[1], itertools.groupby(enumerate(tweet), key=lambda a: x[a[0]]))    
-    
+
+    # Check if character i in the given tweet is a decimal point.
     def is_decimal_place(tweet, i):
+        # We only deal with the simple case where characters on both sides of
+        # the decimal point are numbers.
         return (tweet[i] == "." and
                 i > 0 and
                 tweet[i-1].isdigit() and
                 i + 1 < len(tweet) and
                 tweet[i+1].isdigit())
 
+    # Check if character i in the given tweet is inside an acronym.
     def is_acronym(tweet, i):
-        # First find the boundaries of this token
+        # For our purposes, an acronym is a sequence of alternating letters and
+        # periods, at least four characters long in total. For example: U.S. or
+        # F.D.A.
+        # First find the boundaries of this token.
         start = i
         while start > 0 and (tweet[start-1].isalpha() or tweet[start-1] == "."):
             start -= 1
@@ -155,25 +199,37 @@ def twtt5(tweet):
                 return False
         # If we make it through the check, it's an acronym
         return True
-    # x = map(lambda i: is_acronym(tweet, i), xrange(len(tweet)))
-    # return x
 
+    # Check if the given character is a potential end-of-sentence punctuation
     def is_punctuation(c):
-        return c in ".?!" # Only punctuation we care about for sentence endings
-    
+        return c in ".?!"
+
+    # Check if character i in the given tweet is an end-of-sentence punctuation.
     def is_eos_punctuation(tweet, i):
-        if tweet[i] == '"' and i > 0 and is_eos_punctuation(tweet, i-1):
-            return True # Quotation marks following punctuation should also be
-                        # considered end-of-sentence
+        if tweet[i] in '")' and i > 0 and is_eos_punctuation(tweet, i-1):
+            return True # Quotation marks or closing parens following
+                        # end-of-sentence punctuation should also be considered
+                        # end-of-sentence. This handles cases like: [He said "I
+                        # like it." (And she agreed!)]
         if not is_punctuation(tweet[i]):
             return False
+        # Check if this is a period in the middle of an acronym - that means
+        # it's deinitely not the end of a sentence.
+        is_ac = is_acronym(tweet, i)
+        if is_ac:
+            if i+1 < len(tweet) and (tweet[i+1].isalpha() or tweet[i+1] == "."):
+                return False # Middle of acronym, not end of sentence
+        # Check if this is the period in an abbreviation
         abbrev = get_abbrev(tweet, i)
-        if abbrev:
-            # If it's a non-terminal abbreviation, then this is not eos.
-            if abbrev in non_terminal_abbrevs:
-                return False
-            # Otherwise, check if the next non-whitespace character is uppercase
-            # or not.
+        if abbrev and abbrev in non_terminal_abbrevs:
+            # We define a non-terminal abbreviation to be one which generally
+            # cannot end sentences, and is usually followed by proper nouns. For
+            # example, "Mr." If it's a non-terminal abbreviation, then this is
+            # not eos.
+            return False
+        if abbrev or is_ac:
+            # Otherwise, if we have an abbreviation or the end of an acronym,
+            # check if the next non-whitespace character is uppercase or not.
             for j in xrange(i+1, len(tweet)):
                 if tweet[j].isspace():
                     continue # Skip whitespace
@@ -182,15 +238,16 @@ def twtt5(tweet):
             # the end of sentence.
             return True
         if is_decimal_place(tweet, i):
-            return False
-        if is_acronym(tweet, i):
-            return False
+            return False # periods can also be decimal points! These are not end
+                         # of sentence markers
+        # If we make it through all those special cases, then we think we really
+        # have found the end of the sentence
         return True
 
-    # Takes the text, and partitions it based on split_conditions.
-    # split_conditions is an iterable that is as long as text. Whenever there is a
-    # boundary between nonequal elements in split_conditions, that will result
-    # in a split in the test. For example:
+    # Helper function which takes the text, and partitions it based on
+    # split_conditions. split_conditions is an iterable that is as long as
+    # text. Whenever there is a boundary between nonequal elements in
+    # split_conditions, that will result in a split in the text. For example:
     #    text             = "Hello there"
     #    split_conditions = "aabbbccccdd"
     #    output           = ["He", "llo", " the", "re"]
@@ -199,12 +256,12 @@ def twtt5(tweet):
         groups = itertools.groupby(joined, key=lambda x: x[1])
         groups = [''.join([x[0] for x in g]) for (k, g) in groups]
         return groups
-            
+
+    # Okay, now that we have our helper functions, let's actually do this:
+
     # Create an array telling us which characters are end-of-sentence
     # punctuation.
     eos = [is_eos_punctuation(tweet, i) for i in xrange(len(tweet))]
-    #print tweet
-    #print ''.join(["^" if e else " " for e in eos])
 
     # Now let's split the tweet based on eos punctuation
     splits = split_on(tweet, eos)
@@ -212,6 +269,8 @@ def twtt5(tweet):
     # Okay, now a sentence is defined to be: a string terminated by *one or
     # more* eos punctuation symbols, *possibly separated by whitespace*. That
     # is, the following is only one sentence:
+    # "Argh, why?!?!"
+    # Or even:
     # "Man I hate Mondays . . ."
     # (Ellipsis separated by spaces doesn't get treated as multiple empty
     # sentences)
@@ -236,12 +295,14 @@ def twtt5(tweet):
     # separate using newlines.
     return "\n".join([s.strip() for s in sentences])
 
+# Tokenize tweet.
 # Note, this will tokenize things like !) as single tokens. Example:
 # I like it (a lot!)
 #                 ^^ These two characters are a single token
 # This is because of rule 6 in the handout - multiple punctuation should not be
 # split.
 def twtt7(tweet):
+    # Helper funtion to tokenize a single line
     def tokenize(line):
         # In general, we are just splitting on word boundaries
         toks = [t for t in re.split("(\W+)", line) if t != ""]
@@ -255,7 +316,7 @@ def twtt7(tweet):
                 continue
             # Look for whitespace on either side of the apostrophe:
             if toks[i-1][-1].isspace() or toks[i+1][0].isspace():
-                # Not a clitic - skip
+                # Apostrophe should not be combined with any characters - skip
                 i += 1
                 continue
             # Okay, now look for the special clitic n't
@@ -282,8 +343,11 @@ def twtt7(tweet):
     lines = tweet.split("\n")
     return "\n".join(map(tokenize, lines))
 
+# Tag tweet with parts of speech
 def twtt8(tweet):
     global tagger
+
+    # Helper function to tag a single line.
     def tag(line):
         toks = line.split()
         tags = tagger.tag(toks)
@@ -293,12 +357,13 @@ def twtt8(tweet):
     lines = tweet.split("\n")
     return "\n".join(map(tag, lines))
 
+# Append a polarity marker onto the front of tweet
 def twtt9(tweet, polarity):
     return "<A=%d>\n%s" % (polarity, tweet)
 
 def main(args):
-    global tagger, abbrevs, non_terminal_abbrevs
-    
+    global tagger, html_parser, abbrevs, non_terminal_abbrevs
+
     if len(args) != 4:
         print "Usage: python twtt.py input_file.csv student_number output_file.twt"
         return
@@ -310,30 +375,35 @@ def main(args):
     # Initialize the tagger
     tagger = NLPlib.NLPlib()
 
+    # Initialize the HTML parser
+    html_parser = HTMLParser.HTMLParser()
+
     # Read the abbreviation lists
     abbrev_file = "abbrev.english"
     non_terminal_abbrev_file = "non_terminal_abbrev.english"
     with open(abbrev_file, "r") as f:
         abbrevs = [line.strip() for line in f.readlines()]
-    #print abbrevs
     with open(non_terminal_abbrev_file, "r") as f:
         non_terminal_abbrevs = [line.strip() for line in f.readlines()]
-    #print non_terminal_abbrevs
 
+    # Count the number of lines in the input file - this determines whether we
+    # should process the whole file, or select a subset of lines.
     with open(input_file, "r") as f:
         line_count = sum(1 for line in f)
-    
+
     # Figure out which lines to read
     X = student_number % 80 if line_count > 10000 else 0
     A = 10000*X
     B = 800000 + 10000*X
-    
+
     with open(input_file, "r") as in_file, \
          open(output_file, "w") as out_file:
         for (i, line) in enumerate(in_file):
+            # Check if we should process this line
             if (A <= i < A + 10000) or (B <= i < B + 10000):
+                # Apply each of the preprocessing transformations and write
+                # result to .twtt file.
                 (tweet, polarity) = extract_tweet_and_polarity(line)
-                # Apply each of the preprocessing transformations
                 tweet = twtt1(tweet)
                 tweet = twtt2(tweet)
                 tweet = twtt3(tweet)
@@ -343,6 +413,6 @@ def main(args):
                 tweet = twtt8(tweet)
                 tweet = twtt9(tweet, polarity)
                 out_file.write(tweet + "\n")
-            
+
 if __name__ == "__main__":
     main(sys.argv)
