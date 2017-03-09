7912e170448c5ebd02dc713409442c03625206c4
Young Sandro <youngsan@teach.cs.toronto.edu>
2017-03-09 17:29:30 -0500

    Preprocessing (hopefully) done

--- A2/preprocess.m
+++ A2/preprocess.m
@@ -22,24 +22,58 @@ function outSentence = preprocess( inSentence, language )
   % first, convert the input sentence to lower-case and add sentence marks 
   inSentence = [CSC401_A2_DEFNS.SENTSTART ' ' lower( inSentence ) ' ' CSC401_A2_DEFNS.SENTEND];
 
-  % trim whitespaces down 
-  inSentence = regexprep( inSentence, '\s+', ' '); 
-
   % initialize outSentence
   outSentence = inSentence;
+  
+  % commas, colons and semicolons, parentheses, dashes between parentheses, mathematical operators (e.g., +, -, <, >, =), and quotation marks
 
   % perform language-agnostic changes
   % TODO: your code here
   %    e.g., outSentence = regexprep( outSentence, 'TODO', 'TODO');
+  outSentence = regexprep( outSentence, '([,:;()+<>=*/"])', ' $1 ');
+%   outSentence = regexprep( outSentence, ':', ' : ');
+%   outSentence = regexprep( outSentence, ';', ' ; ');
+%   outSentence = regexprep( outSentence, '(', ' ( ');
+%   outSentence = regexprep( outSentence, ')', ' ) ');
+%   outSentence = regexprep( outSentence, '+', ' + ');
+%   outSentence = regexprep( outSentence, '<', ' < ');
+%   outSentence = regexprep( outSentence, '>', ' > ');
+%   outSentence = regexprep( outSentence, '=', ' = ');
+%   outSentence = regexprep( outSentence, '*', ' * ');
+%   outSentence = regexprep( outSentence, '/', ' / ');
+%   outSentence = regexprep( outSentence, '"', ' " ');
+  
+  % NOTE: does not properly handle nested parens
+  outSentence = regexprep( outSentence, '(\([^)]*[^0-9)]\s*)-(\s*[^0-9])', '$1 - $2');
+  
+  outSentence = regexprep( outSentence, '([.?!]+)(\s*"?\s*SENTEND$)', ' $1 $2');
 
   switch language
    case 'e'
-    % TODO: your code here
+    % Separate clitics from words
+    outSentence = regexprep( outSentence, '([a-z])''', '$1 ''');
+    
+    % Special handling for the n't clitic
+    outSentence = regexprep( outSentence, 'n ''t\>', ' n''t');
 
    case 'f'
-    % TODO: your code here
+    % Handle l' and e-muet
+    outSentence = regexprep( outSentence, '\<([cdjlmnst])''([a-z])', '$1'' $2');
+    
+    % But don't separate d'abord, d'accord, d'ailleurs, d'habitude
+    outSentence = regexprep( outSentence, '\<d'' (abord|accord|ailleurs|habitude)\>', 'd''$1');
+    
+    % Handle qu'
+    outSentence = regexprep( outSentence, '\<qu''([a-z])', 'qu'' $1');
+    
+    % Handle puisque or lorsque contractions
+    outSentence = regexprep( outSentence, '\<(puisqu|lorsqu)''(il|on)\>', '$1'' $2');
+    
 
   end
+  
+  % trim whitespaces down 
+  outSentence = regexprep( outSentence, '\s+', ' '); 
 
   % change unpleasant characters to codes that can be keys in dictionaries
   outSentence = convertSymbols( outSentence );
