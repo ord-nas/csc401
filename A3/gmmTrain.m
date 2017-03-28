f2e2c93ca30be3da8d0dec0f59537fc7014be032
Young Sandro <youngsan@teach.cs.toronto.edu>
2017-03-27 23:35:48 -0400

    Implemented file reading, calling out to unimplemented function gmmEM

--- A3/gmmTrain.m
+++ A3/gmmTrain.m
@@ -15,3 +15,24 @@ function gmms = gmmTrain( dir_train, max_iter, epsilon, M )
 %                                          is a vector
 %                            gmm.cov     : DxDxM matrix of covariances. 
 %                                          (:,:,i) is for i^th mixture
+
+speakers = dir(dir_train);
+counter = 1;
+for s=1:length(speakers)
+    if strcmp(speakers(s).name, '.') || strcmp(speakers(s).name, '..')
+        continue
+    end
+    name = speakers(s).name;
+    all_data = [];
+    data_files = dir([dir_train, filesep, name, filesep, '*.txt']);
+    for f=1:length(data_files)
+        data = dlmread('/u/cs401/speechdata/Training/FCJF0/SA1.mfcc');
+        all_data = [all_data; data];
+    end
+    % fprintf('Speaker %s has data that is %d by %d\n', name, size(all_data, 1), size(all_data, 2));
+    gmm = gmmEM(all_data, max_iter, epsilon, M);
+    gmm.name = name;
+    gmms{counter} = gmm;
+    counter = counter + 1;
+end
+return
